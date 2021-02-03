62a517e9b3f8879fd49665f531368da5481c4034
Nino Fachrurozy <48285366+nfach98@users.noreply.github.com>
2021-02-03 20:56:51 +0700

    fix window decor

--- app/src/main/java/com/example/mediabelajarinteraktif/ActivitySkor.kt
+++ app/src/main/java/com/example/mediabelajarinteraktif/ActivitySkor.kt
@@ -3,6 +3,7 @@ package com.example.mediabelajarinteraktif
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_skor.*
 
 class ActivitySkor : AppCompatActivity() {
@@ -14,5 +15,13 @@ class ActivitySkor : AppCompatActivity() {
             val intent = Intent(this, ActivitySkor::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        this.window.decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 }
